8cfb47430b7706fdce17b19dacf32d7a03213554
perelandrax <luvyum4e@gmail.com>
2020-08-12 22:00:10 +0900

    스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술 업데이트

---
+++ 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술/source/hello-spring/src/main/java/org/example/hellospring/controller/MemberForm.java
@@ -0,0 +1,14 @@
+package org.example.hellospring.controller;
+
+public class MemberForm {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
